104ec3b43bcffcafb925db66405c17db81f1ea64
kisunghyun <132439030+kisunghyun@users.noreply.github.com>
2023-05-03 17:43:23 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[구구단 출력]");
+        int i,j;
+        for (i=01;i<=9;i++) {
+            for(j=1;j<=9;j++) {
+
+
+                for(i=01;i<=9;i++) {
+
+                    System.out.print(i+"X"+j+"="+(i*j)+"	");
+                }
+                System.out.println("");
+            } System.out.println("");
+        }
+    }
+}
